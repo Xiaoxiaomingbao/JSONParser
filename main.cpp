9df4a4cade3731554b09586010fc2e00cd94a40b
Zhiming <xiaoxiaomingbao@gmail.com>
2025-03-07 00:07:10 +0800

    parse json array

--- main.cpp
+++ main.cpp
@@ -43,51 +43,74 @@ std::optional<T> try_parse_num(std::string_view str)
     return std::nullopt;
 }
 
-JSONObject parse(std::string_view json)
+std::pair<JSONObject, size_t> parse(std::string_view json)
 {
-    if (json.empty())
-    {
-        return JSONObject{std::nullptr_t{}};
+    if (json.empty()) {
+        return {JSONObject{std::nullptr_t{}}, 0};
     }
-    if ('0' <= json[0] && json[0] <= '9' || json[0] == '+' || json[0] == '-')
-    {
+
+    if ('0' <= json[0] && json[0] <= '9' || json[0] == '+' || json[0] == '-') {
         std::regex num_re{"[+-]?[0-9]+(\\.[0-9]+)?([eE][+-]?[0-9]+)?"};
         std::cmatch match;
-        if (std::regex_search(json.data(), json.data() + json.size(), match, num_re))
-        {
+        if (std::regex_search(json.data(), json.data() + json.size(), match, num_re)) {
             std::string str = match.str();
-            if (auto num = try_parse_num<int>(str); num.has_value())
-            {
-                return JSONObject{num.value()};
+            if (auto num = try_parse_num<int>(str); num.has_value()) {
+                return {JSONObject{num.value()}, str.size()};
             }
-            if (auto num = try_parse_num<double>(str); num.has_value())
-            {
-                return JSONObject{num.value()};
+            if (auto num = try_parse_num<double>(str); num.has_value()) {
+                return {JSONObject{num.value()}, str.size()};
             }
         }
-    }
-    if ('"' == json[0])
-    {
+    } else if ('"' == json[0]) {
         size_t next_quotation = json.find('"', 1);
         // substr() 第二个参数为长度，不是下标
         // 丢弃输入中自带的双引号
         std::string str{json.substr(1, next_quotation - 1)};
-        return JSONObject{std::move(str)};
+        return {JSONObject{std::move(str)}, next_quotation + 1};
+    } else if (json[0] == '[') {
+        std::vector<JSONObject> res;
+        size_t i;
+        for (i = 1; i < json.size();) {
+            if (json[i] == ']') {
+                i++;
+                break;
+            }
+            // substr() 仅指定起始位置，直到末尾
+            auto [obj, len] = parse(json.substr(i));
+            if (len == 0) {
+                print("Error: invalid json array");
+                break;
+            }
+            res.push_back(std::move(obj));
+            i += len;
+            while (json[i] == ',' || json[i] == ' ') {
+                i++;
+            }
+        }
+        return {JSONObject{std::move(res)}, i};
     }
-    return JSONObject{std::nullptr_t{}};
+    return {JSONObject{std::nullptr_t{}}, 0};
 }
 
 int main()
 {
-    std::string_view json1 = "-7";
-    print(parse(json1));
-    std::string_view json2 = "3.14";
-    print(parse(json2));
-    std::string_view json3 = "-2.5e2";
-    print(parse(json3));
-    std::string_view json4 = "\"hello\"";
-    print(parse(json4));
-    std::string_view json5 = "\"Hello\nWorld!\"";
-    print(parse(json5));
+    std::string_view json_num = "-7";
+    print(parse(json_num).first);
+    json_num = "3.14";
+    print(parse(json_num).first);
+    json_num = "-2.5e2";
+    print(parse(json_num).first);
+
+    std::string_view json_str = "\"hello\"";
+    print(parse(json_str).first);
+    json_str = "\"Hello\nWorld!\"";
+    print(parse(json_str).first);
+
+    std::string_view json_array = "[1, 2, 3]";
+    print(parse(json_array).first);
+    json_array = "[1, [2, 3, 4], 5]";
+    print(parse(json_array).first);
+    json_array = "[123, 1.23, \"hello]world\", -5]";
+    print(parse(json_array).first);
     return 0;
 }
